b5e18a280516cb323c614eb94dd1f66e402c6add
chenzhangxiang <1026161550@qq.com>
2022-01-20 17:01:13 +0800

    fixed vuepress样式引用组件样式

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,7 @@
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+// 组件样式收vuepress主题样式影响，重置这些样式
+import "./style.css";
 import VRender from '../../packages/index.js'
 export default ({
   Vue
